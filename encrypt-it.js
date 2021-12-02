3753854016ccb9bd78ff043c04ff1d29cdca353b
mcmimichan <chan.mcchelle@gmail.com>
2021-12-02 09:46:50 -0800

    Added pseudocode

--- encrypt-it.js
+++ encrypt-it.js
@@ -20,6 +20,10 @@
     // for UI elements on the page.
     const btn = document.getElementById("encrypt-it");
     btn.addEventListener('click', handleClick);
+
+    const btn2 = document.getElementById("reset");
+    btn2.addEventListener('click', handleReset);
+
   }
 
   // Add any other functions in this area (you should not implement your
@@ -28,7 +32,28 @@
   function handleClick() {
     console.log("Button clicked!");
     let txt = document.getElementById("input-text").value;
-    document.getElementById("result").innerHTML = shiftCipher(txt);
+
+    let cipheredText = cipherify(txt);    
+    let sizedText = setSize(cipheredText);
+    let finalText = setCaps(sizedText);
+
+    document.getElementById("result").innerHTML = finalText;
+  }
+
+  function handleReset() {
+    console.log("Resetting...");
+  }
+
+  function cipherify(text){
+    var cipherSelector = document.getElementById("cipher-type");
+    var cipherType = cipherSelector.value;
+
+    if (cipherType == "shift")
+      shiftCipher(text);
+    else if (cipherType == "random")
+      randomCipher(text);
+    
+    return text;
   }
 
   function shiftCipher(text) {
@@ -47,5 +72,51 @@
     }
     return result;
   }
+
+  function randomCipher(text) {
+    return "oops";
+  }
+
+  function setSize(text) {
+    return "double oops";
+  }
+
+  function setCaps(text) {
+    return "triple oops";
+  }
+  
+
+  /******* from Fancify *******
+
+  // Bigger! button: increase font size to 24pt
+  const btn = document.querySelector('button[name="bigger"]');
+
+  function upSize(event) {
+      document.getElementById("txt").style.fontSize= "4em";
+  }
+
+  btn.addEventListener('click', upSize);
+
+  // radio buttons: add/remove styles
+  function fancyOrNot(event) {
+    const rbs = document.querySelectorAll('input[name="js-style"]');
+    for (let rb of rbs) {
+        if (rb.checked) {
+            let selected = rb.value;
+                        
+            if (selected == "fancy") {
+                document.getElementById("txt").style.fontWeight="bold";
+                document.getElementById("txt").style.color="blue";
+                document.getElementById("txt").style.textDecoration="underline";
+            } else if (selected == "boring") {
+                document.getElementById("txt").style.fontWeight="normal";
+                document.getElementById("txt").style.color="black";
+                document.getElementById("txt").style.textDecoration="initial";
+            }
+        }
+    }
+  }
+
+  rb.addEventListener('change', fancyOrNot); */
 
 })();
